a884aa3eeae31d3f98246798d97e19a31b11a5d3
Dandarian <daniel_acba_ssy@mail.ru>
2021-04-10 23:23:54 +0300

    add permissions

--- social_network/api/views.py
+++ social_network/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from . import serializers
 from django.contrib.auth.models import User
 from .models import Post
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
@@ -22,6 +23,8 @@ class PostList(generics.ListCreateAPIView):
     '''
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+    # IsAuthenticatedOrReadOnly встроен в rest_framework
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         ''' Чтобы при создании указывался авторизованный юзер
@@ -35,3 +38,8 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+    # IsOwnerOrReadOnly мы создали в файле permissions
+    # Здесь нужны оба разрешения, поскольку обновлять или удалять пост должен
+    # только залогиненный пользователь, а также его владелец.
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
