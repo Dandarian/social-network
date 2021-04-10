4387512389487f19fdc045a8763bc27488fb54b2
Dandarian <daniel_acba_ssy@mail.ru>
2021-04-10 22:58:14 +0300

    add posts

--- social_network/api/serializers.py
+++ social_network/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Post
 
 # REST Framework Django использует сериализаторы, чтобы переводить наборы
 # запросов и экземпляры моделей в JSON-данные.
@@ -8,7 +9,25 @@ from django.contrib.auth.models import User
 # При необходимости их можно переписать.
 
 
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'body', 'owner']
+
+
 class UserSerializer(serializers.ModelSerializer):
+    # PrimaryKeyRelatedField представляет список публикаций
+    # в этом отношении многие-к-одному (many=True указывает
+    # на то, что постов может быть больше чем один).
+    # Если не задать read_only=True поле posts будет иметь права
+    # записи по умолчанию. Это значит, что будет возможность вручную
+    # задавать список статей, принадлежащих пользователю при его создании.
+    # Вряд ли это желаемое поведение.
+
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'posts']
--- social_network/api/views.py
+++ social_network/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from . import serializers
 from django.contrib.auth.models import User
+from .models import Post
 
 # Create your views here.
 
@@ -13,3 +14,24 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    '''ListCreateAPIView предоставляет распространенный обработчик
+    API-методов: get и post для списка.
+    '''
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        ''' Чтобы при создании указывался авторизованный юзер
+        '''
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    '''RetrieveUpdateDestroyAPIView предоставляет распространенный обработчик
+    API-методов: get, update и delete для одной сущности.
+    '''
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
